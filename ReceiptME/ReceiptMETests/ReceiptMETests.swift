b2136336d4a3adbf153673e0bf11fdcd4831842b
jimmyl123 <j.j.lancaster@wustl.edu>
2025-01-30 15:10:26 -0600

    Xcode setup

---
+++ ReceiptME/ReceiptMETests/ReceiptMETests.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptMETests.swift
+//  ReceiptMETests
+//
+//  Created by Jimmy Lancaster on 1/30/25.
+//
+
+import Testing
+@testable import ReceiptME
+
+struct ReceiptMETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
