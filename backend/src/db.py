e90dbe97a8f51cc7580abbe6fda12c8c0cc96ca0
Grayson Martin <grayson@graysonmartin.net>
2025-02-17 12:58:05 -0600

    begin receipt scanning and db

--- backend/src/db.py
+++ backend/src/db.py
@@ -12,7 +12,9 @@ def connect():
 def init():
 	with connect() as conn:
 		cur = conn.cursor()
-		cur.execute("CREATE TABLE IF NOT EXISTS users (email VARCHAR(255), full_name VARCHAR(255), session_token VARCHAR(255), CONSTRAINT email_unique UNIQUE(email))")
+		cur.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, email VARCHAR(255) UNIQUE, full_name VARCHAR(255), session_token VARCHAR(255))")
+		cur.execute("CREATE TABLE IF NOT EXISTS receipts (id SERIAL PRIMARY KEY, owner_id integer REFERENCES users, date DATE, merchant VARCHAR(255), merchant_address TEXT, merchant_domain VARCHAR(255), payment_method VARCHAR(255), tax DOUBLE PRECISION, clean BOOLEAN)")
+		cur.execute("CREATE TABLE IF NOT EXISTS receipt_items (id SERIAL PRIMARY KEY, receipt_id INTEGER REFERENCES receipts, description VARCHAR(255), price DOUBLE PRECISION)")
 
 def login_user(email, full_name):
 	session_token = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(32))
@@ -20,8 +22,99 @@ def login_user(email, full_name):
 	with connect() as conn:
 		cur = conn.cursor()
 		cur.execute(
-				"INSERT INTO users (email, full_name, session_token) VALUES (%s, %s, %s) ON CONFLICT (email) DO UPDATE SET session_token = %s", 
-				(email, full_name, session_token, session_token)
+			"INSERT INTO users (email, full_name, session_token) VALUES (%s, %s, %s) ON CONFLICT (email) DO UPDATE SET session_token = %s", 
+			(email, full_name, session_token, session_token)
 		)
 
 	return session_token
+
+def check_session_token(token):
+	with connect() as conn:
+		cur = conn.cursor()
+		cur.execute(
+			"SELECT id FROM users WHERE session_token = %s", 
+			(token,)
+		)
+		user_id = cur.fetchone()[0]
+		if user_id is None:
+			return None, False
+		return user_id, True
+
+def create_receipt(user_id, date, merchant, merchant_address, merchant_domain, payment_method, tax, clean):
+	with connect() as conn:
+		cur = conn.cursor()
+		cur.execute(
+			"INSERT INTO receipts (owner_id, date, merchant, merchant_address, merchant_domain, payment_method, tax, clean) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) RETURNING id", 
+			(user_id, date, merchant, merchant_address, merchant_domain, payment_method, tax, clean)
+		)
+		return cur.fetchone()[0]
+
+def create_receipt_item(receipt_id, description, price):
+	with connect() as conn:
+		cur = conn.cursor()
+		cur.execute(
+			"INSERT INTO receipt_items (receipt_id, description, price) VALUES (%s, %s, %s) RETURNING id", 
+			(receipt_id, description, price)
+		)
+		return cur.fetchone()[0]
+
+def list_receipts(user_id):
+	with connect() as conn:
+		cur = conn.cursor()
+		cur.execute(
+			"SELECT id, date, merchant, (SELECT SUM(price) FROM receipt_items WHERE receipt_items.receipt_id = receipts.id) + tax AS total, clean FROM receipts WHERE owner_id = %s ORDER BY date DESC",
+			(user_id,)
+		)
+		rows = cur.fetchall()
+		receipts = []
+		for row in rows:
+			receipts.append({
+				"id": row[0],
+				"date": row[1].__str__(),
+				"merchant": row[2],
+				"total": row[3],
+				"clean": row[4]
+			})
+
+		return receipts
+
+def get_receipt(receipt_id):
+	with connect() as conn:
+		cur = conn.cursor()
+		cur.execute(
+			"SELECT id, owner_id, date, merchant, merchant_address, merchant_domain, payment_method, tax, clean FROM receipts WHERE id = %s",
+			(receipt_id,)
+		)
+		receipt = cur.fetchone()
+
+		if receipt is None:
+			return None
+
+		cur.execute(
+			"SELECT id, description, price FROM receipt_items WHERE receipt_id = %s",
+			(receipt_id,)
+		)
+		items = cur.fetchall()
+
+		out_items = []
+		for item in items:
+			out_items.append({
+				"id": item[0],
+				"description": item[1],
+				"price": item[2]
+			})
+
+		return {
+			"id": receipt[0],
+			"owner_id": receipt[1],
+			"clean": receipt[8],
+			"date": receipt[2].__str__(),
+			"merchant": {
+				"name": receipt[3],
+				"address": receipt[4],
+				"domain": receipt[5]
+			},
+			"payment_method": receipt[6],
+			"items": out_items,
+			"tax": receipt[7]
+		}
--- backend/src/server.py
+++ backend/src/server.py
@@ -1,8 +1,13 @@
 import db
-
 import bottle
 from google.oauth2 import id_token as google_auth
 from google.auth.transport import requests as google_requests
+from PIL import Image
+import pytesseract
+from openai import OpenAI
+import json
+
+openai_client = OpenAI()
 
 @bottle.post("/auth/google/token")
 def	google_auth_token():
@@ -14,13 +19,189 @@ def	google_auth_token():
 	token_data = google_auth.verify_oauth2_token(token_str, google_requests.Request(), "650383131525-anbr9ft0hfl03jbhbl21sokpgchc12tg.apps.googleusercontent.com")
 
 	if not token_data["email_verified"]:
-			bottle.response.status = 403
-			return "Unauthorized"
+		bottle.response.status = 403
+		return "Unauthorized"
 
 	session_token = db.login_user(token_data["email"], token_data["name"])
 	return {
 		"session": session_token
 	}
+
+@bottle.get("/receipts")
+def get_receipts():
+	user_id, ok = db.check_session_token(bottle.request.get_header("Authorization"))
+	if not ok:
+		bottle.response.status = 403
+		return "Unauthorized"
+
+	return {
+		"receipts": db.list_receipts(user_id)
+	}
+
+@bottle.get("/receipts/<receipt_id>")
+def get_receipt(receipt_id):
+	user_id, ok = db.check_session_token(bottle.request.get_header("Authorization"))
+	if not ok:
+		bottle.response.status = 403
+		return "Unauthorized"
+
+	receipt = db.get_receipt(receipt_id)
+
+	if receipt is None:
+		bottle.response.status = 404
+		return "Not found"
+
+	if receipt["owner_id"] != user_id:
+		bottle.response.status = 401
+		return "Forbidden"
+
+	return receipt
+
+
+@bottle.post("/receipts/auto")
+def add_receipt_auto():
+	user_id, ok = db.check_session_token(bottle.request.get_header("Authorization"))
+	if not ok:
+		bottle.response.status = 403
+		return "Unauthorized"
+
+	img = Image.open(bottle.request.body)
+	receipt_text = pytesseract.image_to_string(img, lang="eng", config="--psm 6")
+
+	receipt_text = clean_receipt_text(receipt_text)
+
+	res = openai_client.chat.completions.create(
+		model="gpt-4o-mini",
+		messages=[{
+			"role": "system",
+			"content": [{
+				"type": "text",
+				"text": "You will be provided with the OCR extracted text from a receipt. Parse the text and provide the requested JSON formatted output. OCR outputs are inherently messy, so extract only the relevant information."
+			}]
+		}, {
+			"role": "user",
+			"content": [{
+				"type": "text",
+				"text": receipt_text
+			}]
+		}],
+		response_format={
+			"type": "json_schema",
+			"json_schema": {
+				"name": "payment_receipt",
+				"strict": False,
+				"schema": {
+					"type": "object",
+					"properties": {
+						"name": {
+							"type": "string",
+							"description": "Name of the merchant"
+						},
+						"date": {
+							"type": "string",
+							"description": "The date on the receipt, in the format MM-DD-YYYY"
+						},
+						"merchant_address": {
+							"type": "string",
+							"description": "Address of the merchant"
+						},
+						"merchant_website": {
+							"type": "string",
+							"description": "Merchant's domain name"
+						},
+						"items": {
+							"type": "array",
+							"description": "List of items included in the payment receipt.",
+							"items": {
+								"type": "object",
+								"properties": {
+									"description": {
+										"type": "string",
+										"description": "A brief description of the item. Remove any nonsensical or unnecessary words and normalize capitalization."
+									},
+									"cost": {
+										"type": "number",
+										"description": "Cost of the individual item."
+									}
+								},
+								"required": [
+									"description",
+									"cost"
+								],
+								"additionalProperties": False
+							}
+						},
+						"subtotal": {
+							"type": "number",
+							"description": "Total cost of items before any additional charges."
+						},
+						"total": {
+							"type": "number",
+							"description": "Total amount due including any additional charges or taxes."
+						},
+						"payment_method": {
+							"type": "string",
+							"description": "Description of the payment method (network and card number if credit card), modality name otherwise"
+						}
+					},
+					"required": [
+						"items",
+						"subtotal",
+						"total"
+					],
+					"additionalProperties": False
+				}
+			}
+		}
+	)
+	try:
+		receipt_json = json.loads(res.choices[0].message.content)
+	except:
+		return {
+			"success": False
+		}
+
+	required_fields = ["items", "subtotal", "date", "total", "name"]
+	for required_field in required_fields:
+		if required_field not in receipt_json:
+			print("missing field: ", required_field)
+			return False
+
+	receipt_id = save_receipt(user_id, receipt_json)
+
+	return {
+		"success": True,
+		"receipt_id": receipt_id
+	}
+
+def clean_receipt_text(text):
+	lines = text.split("\n")
+	out = ""
+	for line in lines:
+		if "@" not in line:
+			out += line + "\n"
+	return out
+
+def receipt_verify(data):
+	items_subtotal = 0
+	for item in data["items"]:
+		if "cost" not in item:
+			print("item has no cost")
+			return False
+		items_subtotal += item["cost"]
+
+	if data["subtotal"] != round(items_subtotal, 2):
+		print("items dont sum to subtotal")
+		return False
+	
+	return True
+
+def save_receipt(user_id, data):
+	tax = round(data["total"] - data["subtotal"], 2)
+	receipt_id = db.create_receipt(user_id, data["date"], data["name"], data["merchant_address"] or "", data["merchant_website"] or "", data["payment_method"] or "", tax, receipt_verify(data))
+	for item in data["items"]:
+		db.create_receipt_item(receipt_id, item["description"], round(item["cost"], 2))
+	return receipt_id
 
 db.init()
 bottle.run(host='0.0.0.0', port=8080, debug=False)
